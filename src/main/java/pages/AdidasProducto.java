a3ee4fe3e375a8d877d5b07695b437e7047f1cbe
HectorRubio2 <66961920+hector26rubio2@users.noreply.github.com>
2021-12-08 11:16:13 -0500

    redme
    imagnes palra explicamr mejor el readme

--- src/main/java/pages/AdidasProducto.java
+++ src/main/java/pages/AdidasProducto.java
@@ -11,6 +11,9 @@ public class AdidasProducto {
     }
 
     public void setTxtProducto(String producto) {
-        this.txtProducto = By.xpath("//div[@class ='product-description___2cJO2']/h1[@data-auto-id='product-title' and @class='gl-heading gl-heading--regular gl-heading--italic name___1EbZs']//span[contains(text(), '"+producto+"')]");
+        this.txtProducto = By.xpath("//div[@class ='product-description___2cJO2']" +
+                "/h1[@data-auto-id='product-title'" +
+                " and @class='gl-heading gl-heading--regular gl-heading--italic name___1EbZs']" +
+                "//span[contains(text(), '"+producto+"')]");
     }
 }
